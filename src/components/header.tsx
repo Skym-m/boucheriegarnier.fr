e3fd8e4106047ec4489cda82146ea9c3510bf255
Skym <yannis.perrier@icloud.com>
2024-09-30 21:49:46 +0200

    Update en Next.js

---
+++ src/components/header.tsx
@@ -0,0 +1,31 @@
+const header = () => {
+
+    return (
+        <header>
+        <nav className="nav">
+            <ul id="list" className="flex items-center justify-center fixed z-50 w-full h-16 bg-black text-white">
+                <li>
+                    <a href="/">ACCUEIL</a>
+                </li>
+                <li>
+                    <a href="/">TEST</a>
+                </li>
+                <li>
+                    <a href="/">LOREM</a>
+                </li>
+                <li>
+                    <a href="/">IPSUM</a>
+                </li>
+                <li>
+                    <a href="/">DES</a>
+                </li>
+                <li>
+                    <a href="/">ILES</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
+
+export default header
